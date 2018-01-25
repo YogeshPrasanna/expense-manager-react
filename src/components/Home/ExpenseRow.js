38696006afeeed96c97880f8309ef432ed4313b3
YogeshPrasanna <yogesh15031993@gmail.com>
2018-01-25 15:13:24 +0530

    add delete feature on each expense

--- src/components/Home/ExpenseRow.js
+++ src/components/Home/ExpenseRow.js
@@ -1,16 +1,31 @@
-import React from 'react'
+import React , { Component } from 'react'
+import * as firebase from '../../firebase/firebase'
 
-const ExpenseRow = (props) => {
+class ExpenseRow  extends Component {
 
-    return (
-        <tr key={props.expenseId} id={props.expenseId}>
-            <td>{props.num + 1}</td>
-            <td>{props.expense.value.date}</td>
-            <td>{props.expense.value.expense}</td>
-            <td>{props.expense.value.category}</td>
-            <td>{props.expense.value.comments}</td>            
-        </tr>
-    )
+    constructor(props){
+        super(props)
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick(e){
+        console.log("clicked", this.props.expenseId)
+        firebase.db.ref(`expenses/${this.props.expenseId}`).remove()
+    }
+
+    render(){
+        return (
+            <tr key={this.props.expenseId} id={this.props.expenseId}>
+                <td>{this.props.num + 1}</td>
+                <td>{this.props.expense.value.date}</td>
+                <td>{this.props.expense.value.expense}</td>
+                <td>{this.props.expense.value.category}</td>
+                <td>{this.props.expense.value.comments}</td>
+                <td><button onClick={this.handleClick}>Delete</button></td>
+            </tr>
+        )
+    }
 }
 
 export default ExpenseRow
