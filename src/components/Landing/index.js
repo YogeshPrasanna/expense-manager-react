445b81d34f0efa239911c01798ae809a9b4e7eda
YogeshPrasanna <yogesh15031993@gmail.com>
2018-01-29 15:53:03 +0530

    add content to landing page

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 
 const LandingPage = () =>
-    <div>
-        <h1>Landing Page</h1>
+    <div className="landing">
+        <span>" Expense Manager which takes note of all your daily expenses "</span>
+        <p> Sign up to create an account - and start managing your expenses </p>
     </div>
 
 export default LandingPage;
