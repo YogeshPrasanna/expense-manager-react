8647a30ea3216c8fb6fee98f40bcc5b950da7f2f
YogeshPrasanna <yogesh15031993@gmail.com>
2018-02-01 11:13:30 +0530

    add week card and change card colors

--- src/components/Home/Cards.js
+++ src/components/Home/Cards.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import moment from 'moment'
 
 const Cards = (props) => {
 
@@ -7,6 +8,7 @@ const Cards = (props) => {
     var totalExpenses = 0;
     var totalExpensesThisMonth = 0;
     var totalExpensesToday = 0;
+    var totalExpensesThisWeek = 0;
 
     if (!expenses) {
         return <tr><td> Loading ... </td></tr>
@@ -24,10 +26,10 @@ const Cards = (props) => {
         var thisUsersExpenses = eachExpense.filter((elem) => elem.value.uid === currentUser.uid);
         var thisUsersExpensesThisMonth = eachExpense.filter((elem) => elem.value.uid === currentUser.uid && new Date(elem.value.date).getMonth() === new Date().getMonth())
         var thisUsersExpensesToday = thisUsersExpensesThisMonth.filter((elem) => new Date(elem.value.date).getDate() === new Date().getDate())          
+        var thisUsersExpensesThisWeek = eachExpense.filter((elem) => elem.value.uid === currentUser.uid && (moment(elem.value.date , 'MM/DD/YYYY').week() === moment(moment(new Date()) , 'MM/DD/YYYY').week()))
 
-        console.log("This users expenses : ", thisUsersExpensesThisMonth)
+        console.log("This users expenses this week : ", thisUsersExpensesThisWeek)
 
-        
 
         if (thisUsersExpenses.length > 1) {
             totalExpenses = thisUsersExpenses.map((elem) => {
@@ -60,11 +62,23 @@ const Cards = (props) => {
         } else {
             totalExpensesToday = 0
         }
+
+        if (thisUsersExpensesThisWeek.length > 1) {
+            totalExpensesThisWeek = thisUsersExpensesThisWeek.map((elem) => {
+                return Number(elem.value.expense)
+            }).reduce((prev, cur) => prev + cur)
+        } else if (thisUsersExpensesThisWeek.length === 1) {
+            totalExpensesThisWeek = thisUsersExpensesThisWeek[0].value.expense
+        } else {
+            totalExpensesThisWeek = 0
+        }
+
+
     }
 
     return (
         <div className="row">
-            <div className="col-sm-4">
+            <div className="col-sm-3">
                 <div className="card card1">
                     <div className="card-block">
                         <h3 className="card-title">Overall Money Spent</h3>
@@ -72,19 +86,27 @@ const Cards = (props) => {
                     </div>
                 </div>
             </div>
-            <div className="col-sm-4">
+            <div className="col-sm-3">
                 <div className="card card2">
                     <div className="card-block">
-                        <h3 className="card-title">Money Spent This Month</h3>
+                        <h3 className="card-title">This Month</h3>
                         <p className="card-text"><i className="fa fa-inr" aria-hidden="true"></i> {totalExpensesThisMonth}</p>
                     </div>
                 </div>
             </div>
-            <div className="col-sm-4">
+            <div className="col-sm-3">
                 <div className="card card3">
                     <div className="card-block">
-                        <h3 className="card-title">Money Spent Today</h3>
+                        <h3 className="card-title">Today</h3>
                         <p className="card-text"><i className="fa fa-inr" aria-hidden="true"></i> {totalExpensesToday}</p>
+                    </div>
+                </div>
+            </div>
+            <div className="col-sm-3">
+                <div className="card card4">
+                    <div className="card-block">
+                        <h3 className="card-title">This Week</h3>
+                        <p className="card-text"><i className="fa fa-inr" aria-hidden="true"></i> {totalExpensesThisWeek}</p>
                     </div>
                 </div>
             </div>
