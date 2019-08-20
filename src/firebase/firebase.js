3be3de7ad330583967c6f602417212f7aa04e311
seanchok <seanchok@gmail.com>
2019-08-21 07:32:09 +0800

    setup firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,11 +2,12 @@ import * as firebase from "firebase";
 
 const config = {
     apiKey: `${process.env.REACT_APP_FIREBASE_API_KEY}`,
-    authDomain: "expense-manager-react.firebaseapp.com",
-    databaseURL: "https://expense-manager-react.firebaseio.com",
-    projectId: "expense-manager-react",
-    storageBucket: "expense-manager-react.appspot.com",
-    messagingSenderId: "847044394910"
+    authDomain: "expense-manager-712af.firebaseapp.com",
+    databaseURL: "https://expense-manager-712af.firebaseio.com",
+    projectId: "expense-manager-712af",
+    storageBucket: "",
+    messagingSenderId: "438737817312",
+    appId: "1:438737817312:web:daecd20d3d63bc9e"
 };
 
 if (!firebase.apps.length) {
