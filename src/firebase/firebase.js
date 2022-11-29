178fc3832d4ac19999f1a6643734ca3478825b93
Fahad <ft.thakur1@gmail.com>
2022-11-29 22:18:32 +0800

    Migrated basic read/write functions to firestore

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,9 +2,10 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import "firebase/compat/firestore";
 import "firebase/compat/database";
+import { getFirestore } from "firebase/firestore";
 
 const config = {
-  apiKey: "AIzaSyBlBNI2nCUe7E5SRZfY1FvqUcPRobyBfBc",
+  apiKey: `${process.env.REACT_APP_FIREBASE_API_KEY}`,
   authDomain: "expense-manager-43d96.firebaseapp.com",
   databaseURL:
     "https://expense-manager-43d96-default-rtdb.asia-southeast1.firebasedatabase.app",
@@ -15,11 +16,9 @@ const config = {
   measurementId: "G-2Q0196XQ95",
 };
 
-if (!firebase.apps.length) {
-  firebase.initializeApp(config);
-}
+const app = firebase.initializeApp(config);
 
-const db = firebase.database();
+const db = getFirestore(app);
 const auth = firebase.auth();
 
 export { auth, db };
