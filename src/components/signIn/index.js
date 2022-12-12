e4a8e1912154dd0bc3688eac658d719dd6b003d6
Muhammad Hafiz <hafizemail135@gmail.com>
2022-12-13 03:53:45 +0800

    Final Test For Auto Deploy Master

--- src/components/signIn/index.js
+++ src/components/signIn/index.js
@@ -195,7 +195,7 @@ class SignInForm extends Component {
               </span>
               <hr />
               <p>
-                Sign up to create an account - and start managing your expenses Test 2
+                Sign up to create an account - and start managing your expenses
               </p>
               <hr />
               <iframe
