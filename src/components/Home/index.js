6553a649c8e04a9dff166c6efab420d467cf799d
Muhammad Hafiz <hafizemail135@gmail.com>
2022-12-13 11:28:21 +0800

    Testing new bootstrap modal

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 
 import ExpenseTable from "./ExpenseTable.js";
 import AddExpensePopup from "./AddExpensePopup";
@@ -10,129 +10,191 @@ import MobileExpenseTable from "./MobileExpenseTable";
 
 import * as analytics from "./../../analytics/analytics";
 
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+
+export const NewAddExpenseModal = (props) => {
+  const [open, setOpen] = useState(props.openModal);
+  const nightModePopup = {
+    backgroundColor: props.settings
+      ? props.settings.mode === "night"
+        ? "#857861"
+        : "#fff"
+      : "#fff",
+  };
+
+  return <></>;
+};
+
 class HomePage extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = { showPopup: false, convertedCurrency: null };
-    }
+    this.state = {
+      showPopup: false,
+      convertedCurrency: null,
+      newModal: false,
+    };
+  }
 
-    togglePopup() {
-        this.setState({
-            showPopup: !this.state.showPopup
+  togglePopup() {
+    this.setState({
+      showPopup: !this.state.showPopup,
+    });
+  }
+
+  toggleNewModal() {
+    this.setState({
+      newModal: !this.state.newModal,
+    });
+  }
+
+  componentDidMount() {
+    analytics.initGA();
+    analytics.logPageView();
+
+    // if travel mode then convert currency else set to 1
+    if (this.props.settings && this.props.settings.travelMode === "on") {
+      function returnCur(cur) {
+        switch (cur) {
+          case "Indian Rupees":
+            return "INR";
+          case "US Dollars":
+            return "USD";
+          case "Pounds":
+            return "EUR";
+          case "Euro":
+            return "EUR";
+          case "Yen":
+            return "YER";
+          default:
+            return "INR";
+        }
+      }
+
+      const fromcur = returnCur(this.props.settings.fromCurrency);
+      const tocur = returnCur(this.props.settings.currency);
+
+      fetch(
+        `https://free.currencyconverterapi.com/api/v5/convert?q=${fromcur}_${tocur}&compact=y&apiKey=${process.env.REACT_APP_FREE_CURRENCY_CONVERTER_API_KEY}`
+      )
+        .then((resp) => resp.json()) // Transform the data into json
+        .then((data) => {
+          this.setState({ convertedCurrency: Object.values(data)[0].val });
+        })
+        .catch(() => {
+          alert(
+            "Some Problem with the currency converter api. Values will Fallback to default currency"
+          );
+          this.setState({ convertedCurrency: 1 });
         });
+    } else {
+      this.setState({ convertedCurrency: 1 });
     }
+  }
 
-    componentDidMount() {
-        analytics.initGA();
-        analytics.logPageView();
+  render() {
+    const styleFromSettings = {
+      fontFamily: this.props.settings ? this.props.settings.font : "sans-serif",
+      backgroundColor: this.props.settings
+        ? this.props.settings.mode === "night"
+          ? "#484842"
+          : "auto"
+        : "auto",
+      minHeight: "91vh",
+    };
 
-        // if travel mode then convert currency else set to 1
-        if (this.props.settings && this.props.settings.travelMode === "on") {
-            function returnCur(cur) {
-                switch (cur) {
-                    case "Indian Rupees":
-                        return "INR";
-                    case "US Dollars":
-                        return "USD";
-                    case "Pounds":
-                        return "EUR";
-                    case "Euro":
-                        return "EUR";
-                    case "Yen":
-                        return "YER";
-                    default:
-                        return "INR";
-                }
-            }
+    if (this.props.settings && this.props.cards) {
+      return (
+        <div>
+          <div className="col-sm-12 mobileNoPadding" style={styleFromSettings}>
+            <Cards
+              expenses={this.props.expenses}
+              authUser={this.props.user}
+              settings={this.props.settings}
+              cards={this.props.cards}
+            />
 
-            const fromcur = returnCur(this.props.settings.fromCurrency);
-            const tocur = returnCur(this.props.settings.currency);
+            <GenerateExcel
+              expenses={this.props.expenses}
+              authUser={this.props.user}
+              settings={this.props.settings}
+            />
+            {this.state.convertedCurrency ? (
+              window.screen.width > 720 ? (
+                <ExpenseTable
+                  expenses={this.props.expenses}
+                  authUser={this.props.user}
+                  settings={this.props.settings}
+                  convertedCurrency={this.state.convertedCurrency}
+                />
+              ) : (
+                <MobileExpenseTable
+                  expenses={this.props.expenses}
+                  authUser={this.props.user}
+                  settings={this.props.settings}
+                  convertedCurrency={this.state.convertedCurrency}
+                />
+              )
+            ) : (
+              <Loader />
+            )}
+          </div>
+          <button
+            className="addexpense-btn"
+            onClick={this.toggleNewModal.bind(this)}
+          >
+            <i className="fa fa-plus-circle fa-5x" aria-hidden="true" />
+          </button>
 
-            fetch(`https://free.currencyconverterapi.com/api/v5/convert?q=${fromcur}_${tocur}&compact=y&apiKey=${process.env.REACT_APP_FREE_CURRENCY_CONVERTER_API_KEY}`)
-                .then(resp => resp.json()) // Transform the data into json
-                .then(data => {
-                    this.setState({ convertedCurrency: Object.values(data)[0].val });
-                })
-                .catch(() => {
-                    alert("Some Problem with the currency converter api. Values will Fallback to default currency");
-                    this.setState({ convertedCurrency: 1 });
-                });
-        } else {
-            this.setState({ convertedCurrency: 1 });
-        }
+          {this.state.newModal ? (
+            this.state.convertedCurrency ? (
+              <NewAddExpenseModal
+                user={this.props.user}
+                openModal={this.state.newModal}
+                closePopup={this.toggleNewModal.bind(this)}
+                settings={this.props.settings}
+                convertedCurrency={this.state.convertedCurrency}
+              />
+            ) : (
+              <div style={styleFromSettings}>
+                <Loader />
+              </div>
+            )
+          ) : null}
+
+          {/* Open Form */}
+          <button
+            className="addexpense-btn"
+            onClick={this.togglePopup.bind(this)}
+            id="addExpense"
+          >
+            <i className="fa fa-plus-circle fa-5x" aria-hidden="true" />
+          </button>
+          {this.state.showPopup ? (
+            this.state.convertedCurrency ? (
+              <AddExpensePopup
+                user={this.props.user}
+                closePopup={this.togglePopup.bind(this)}
+                settings={this.props.settings}
+                convertedCurrency={this.state.convertedCurrency}
+              />
+            ) : (
+              <div style={styleFromSettings}>
+                <Loader />
+              </div>
+            )
+          ) : null}
+        </div>
+      );
+    } else {
+      return (
+        <div style={styleFromSettings}>
+          <Loader />
+        </div>
+      );
     }
-
-    render() {
-        const styleFromSettings = {
-            fontFamily: this.props.settings ? this.props.settings.font : "sans-serif",
-            backgroundColor: this.props.settings ? (this.props.settings.mode === "night" ? "#484842" : "auto") : "auto",
-            minHeight: "91vh"
-        };
-
-        if (this.props.settings && this.props.cards) {
-
-            return (
-                <div>
-                    <div className="col-sm-12 mobileNoPadding" style={styleFromSettings}>
-                        <Cards
-                            expenses={this.props.expenses}
-                            authUser={this.props.user}
-                            settings={this.props.settings}
-                            cards={this.props.cards}
-                        />
-
-                        <GenerateExcel
-                            expenses={this.props.expenses}
-                            authUser={this.props.user}
-                            settings={this.props.settings}
-                        />
-                        {this.state.convertedCurrency ? (
-                            window.screen.width > 720 ? (
-                                <ExpenseTable
-                                    expenses={this.props.expenses}
-                                    authUser={this.props.user}
-                                    settings={this.props.settings}
-                                    convertedCurrency={this.state.convertedCurrency}
-                                />
-                            ) : (
-                                    <MobileExpenseTable
-                                        expenses={this.props.expenses}
-                                        authUser={this.props.user}
-                                        settings={this.props.settings}
-                                        convertedCurrency={this.state.convertedCurrency}
-                                    />
-                                )
-                        ) : (
-                                <Loader />
-                            )}
-                    </div>
-                    <button className="addexpense-btn" onClick={this.togglePopup.bind(this)} id="addExpense">
-                        <i className="fa fa-plus-circle fa-5x" aria-hidden="true" />
-                    </button>
-                    {this.state.showPopup ? (
-                        this.state.convertedCurrency ? (
-                            <AddExpensePopup
-                                user={this.props.user}
-                                closePopup={this.togglePopup.bind(this)}
-                                settings={this.props.settings}
-                                convertedCurrency={this.state.convertedCurrency}
-                            />
-                        ) : (
-                                <div style={styleFromSettings}><Loader /></div>
-
-                            )
-                    ) : null}
-                </div>
-            );
-        } else {
-            return (
-                <div style={styleFromSettings}>
-                    <Loader />
-                </div>
-            );
-        }
-    }
+  }
 }
 
 export default HomePage;
