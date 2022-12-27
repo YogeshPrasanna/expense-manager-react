1b6a565fec21ea8ce001d9b6250729978b43d454
Muhammad Hafiz <hafizemail135@gmail.com>
2022-12-27 12:11:03 +0800

    Modal Example Loan

--- src/components/Loan/index.js
+++ src/components/Loan/index.js
@@ -1,5 +1,6 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import AddLoanPopup from "./AddLoanPopup";
+import AddLoanForm from "./AddLoanForm";
 import LoanTable from "./LoanTable";
 import GenerateExcel from "./GenerateExcel";
 import Cards from "./Cards";
@@ -8,102 +9,111 @@ import BarChartAllMonths from "./BarChartAllMonths";
 
 import * as analytics from "./../../analytics/analytics";
 
+import { Button, Modal } from "react-bootstrap";
+
 class LoanPage extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = { showPopup: false };
-        this.state.search = "";
+    this.state = { showPopup: false };
+    this.state.search = "";
+  }
+
+  togglePopup() {
+    this.setState({
+      showPopup: !this.state.showPopup,
+    });
+  }
+
+  componentDidMount() {
+    analytics.initGA();
+    analytics.logPageView();
+  }
+
+  handleChange(e) {
+    // If you are using babel, you can use ES 6 dictionary syntax { [e.target.name] = e.target.value }
+    var change = {};
+    change[e.target.name] = e.target.value;
+    this.setState(change);
+  }
+
+  changePage() {
+    console.log();
+  }
+
+  filterLoan() {
+    if (this.state.search == "" && this.props.loans) {
+      return this.props.loans;
+    } else if (this.state.search != "") {
+      var loans = Object.values(this.props.loans);
+      return loans.filter((loan) => {
+        return (
+          loan.person.toLowerCase().includes(this.state.search.toLowerCase()) ||
+          loan.reason.toLowerCase().includes(this.state.search.toLowerCase())
+        );
+      });
     }
+  }
 
-    togglePopup() {
-        this.setState({
-            showPopup: !this.state.showPopup
-        });
-    }
+  render() {
+    const styleFromSettings = {
+      fontFamily: this.props.settings ? this.props.settings.font : "sans-serif",
+      backgroundColor: this.props.settings
+        ? this.props.settings.mode === "night"
+          ? "#484842"
+          : "auto"
+        : "auto",
+      minHeight: "91vh",
+    };
 
-    componentDidMount() {
-        analytics.initGA();
-        analytics.logPageView();
-    }
-
-    handleChange(e) {
-        // If you are using babel, you can use ES 6 dictionary syntax { [e.target.name] = e.target.value }
-        var change = {};
-        change[e.target.name] = e.target.value;
-        this.setState(change);
-    }
-
-    changePage() {
-        console.log()
-    }
-
-    filterLoan() {
-        if(this.state.search == "" && this.props.loans) {
-            return this.props.loans;
-        } else if (this.state.search != "") {
-            var loans = Object.values(this.props.loans)
-            return loans.filter((loan) => {
-                return (loan.person.toLowerCase().includes(this.state.search.toLowerCase()) || loan.reason.toLowerCase().includes(this.state.search.toLowerCase()))
-            });
-        }
-    }
-
-    render() {
-        const styleFromSettings = {
-            fontFamily: this.props.settings ? this.props.settings.font : "sans-serif",
-            backgroundColor: this.props.settings ? (this.props.settings.mode === "night" ? "#484842" : "auto") : "auto",
-            minHeight: "91vh"
-        };
-
-        if (this.props.settings && this.props.cards) {
-            return (
-                <div className="container-fluid" style={styleFromSettings}>
-                    <div className="row align-items-center">
-                        <div className="col-sm-3 mobileNoPadding">
-                            <Cards
-                                loans={this.props.loans}
-                                authUser={this.props.user}
-                                settings={this.props.settings}
-                                cards={this.props.cards}
-                            />
-                        </div>
-                        <div className="col-sm-9 mobileNoPadding">
-                            <BarChartAllMonths 
-                                loans={this.props.loans} 
-                                authUser={this.props.user} 
-                                settings={this.props.settings} 
-                            />
-                        </div>
-                    </div>
-                    <div className="row justify-content-end">
-                    {/* <div className="form-group row"> */}
-                        <div className="col-12 col-md-4 mt-2">                           
-                        <input
-                                type="text"
-                                name="search"
-                                maxLength={50}
-                                value={this.state.search}
-                                onChange={this.handleChange.bind(this)}
-                                placeholder="🔍︎ search by person or reason"
-                                className="form-control"
-                            />
-                        </div>
-                    {/* </div> */}
-                        <div className="col-sm-12 mobileNoPadding">
-                            <GenerateExcel
-                                loans={this.props.loans}
-                                authUser={this.props.user}
-                                settings={this.props.settings}
-                            />
-                            <LoanTable
-                                loans={this.filterLoan()}
-                                authUser={this.props.user}
-                                settings={this.props.settings}
-                            />
-                        </div>
-                    </div>
-                    {/* <div className="row">
+    if (this.props.settings && this.props.cards) {
+      return (
+        <div className="container-fluid" style={styleFromSettings}>
+          <div className="row align-items-center">
+            <div className="col-sm-3 mobileNoPadding">
+              <Cards
+                loans={this.props.loans}
+                authUser={this.props.user}
+                settings={this.props.settings}
+                cards={this.props.cards}
+              />
+            </div>
+            <div className="col-sm-9 mobileNoPadding">
+              <BarChartAllMonths
+                loans={this.props.loans}
+                authUser={this.props.user}
+                settings={this.props.settings}
+              />
+            </div>
+          </div>
+          <div className="row justify-content-end">
+            {/* <div className="form-group row"> */}
+            <div className="col-12 col-md-4 mt-2">
+              <input
+                type="text"
+                name="search"
+                maxLength={50}
+                value={this.state.search}
+                onChange={this.handleChange.bind(this)}
+                placeholder="🔍︎ search by person or reason"
+                className="form-control"
+              />
+            </div>
+            {/* </div> */}
+            <div className="col-sm-12 mobileNoPadding">
+              <GenerateExcel
+                loans={this.props.loans}
+                authUser={this.props.user}
+                settings={this.props.settings}
+              />
+              <LoanTable
+                loans={this.filterLoan()}
+                authUser={this.props.user}
+                settings={this.props.settings}
+              />
+            </div>
+          </div>
+          {/* <div className="row">
                             <ul class="pagination mx-auto">
                                 <li class="page-item">
                                 <a class="page-link" href="#" aria-label="Previous">
@@ -121,28 +131,65 @@ class LoanPage extends Component {
                             </ul>
                         </div> */}
 
-                    <button className="addloan-btn" onClick={this.togglePopup.bind(this)} id="addLoan">
-                        <i className="fa fa-plus-circle fa-5x" aria-hidden="true" />
-                    </button>
+          <button
+            className="addloan-btn"
+            onClick={this.togglePopup.bind(this)}
+            id="addLoan"
+          >
+            <i className="fa fa-plus-circle fa-5x" aria-hidden="true" />
+          </button>
 
-                    {this.state.showPopup ? (
-                        <AddLoanPopup
-                            user={this.props.user}
-                            openModal={this.state.showPopup}
-                            togglePopout={this.togglePopup.bind(this)}
-                            settings={this.props.settings}
-                        />
-                    ) : null}
-                </div>
-            );
-        } else {
-            return (
-                <div>
-                    <Loader />
-                </div>
-            );
-        }
+          {this.state.showPopup ? (
+            // <AddLoanPopup
+            //   user={this.props.user}
+            //   openModal={this.state.showPopup}
+            //   togglePopout={this.togglePopup.bind(this)}
+            //   settings={this.props.settings}
+            // />
+            <TestModal
+              user={this.props.user}
+              openModal={this.state.showPopup}
+              togglePopout={this.togglePopup.bind(this)}
+              settings={this.props.settings}
+            />
+          ) : null}
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <Loader />
+        </div>
+      );
     }
+  }
 }
 
 export default LoanPage;
+
+function TestModal(props) {
+  const [open, setOpen] = useState(props.openModal);
+
+  return (
+    <>
+      <Modal show={open} onHide={props.togglePopout}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {/* Put Your Form Component Here */}
+          <AddLoanForm user={props.user} settings={props.settings} />
+          {/* Component Form */}
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={props.togglePopout}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={props.togglePopout}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
